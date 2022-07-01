85f8946b75b7a25c1d72fa2f482b587fa3d5bfb3
nirmal1142 <ribadiyanirmal22@gmail.com>
2022-07-01 11:26:57 +0530

    add new changes

--- djangoauthapi/wsgi.py
+++ djangoauthapi/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
+# from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoauthapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoauthapi.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root="/path/to/static/files")
-application.add_files("/path/to/more/static/files", prefix="more-files/")
+# application = WhiteNoise(application, root="/path/to/static/files")
+# application.add_files("/path/to/more/static/files", prefix="more-files/")
